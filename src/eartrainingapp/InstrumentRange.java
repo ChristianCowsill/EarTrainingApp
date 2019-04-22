2dcdcb1b99a522d783e2452c5805347165fb9c51
ChristianCowsill <chris_cowsill@hotmail.com>
2019-04-21 23:38:32 -0400

    Added Instrument selection

--- src/eartrainingapp/InstrumentRange.java
+++ src/eartrainingapp/InstrumentRange.java
@@ -7,21 +7,24 @@ package eartrainingapp;
  */
 public enum InstrumentRange {
     
-    GUITAR(40, 76, 27, 57),
-    BASS_GUITAR(28, 55, 35, 45),
-    PIANO(21, 108, 1, 60)
+    GUITAR(40, 76, 27, 57, "Guitar"),
+    BASS_GUITAR(28, 55, 35, 45, "Four String Bass"),
+    PIANO(21, 108, 1, 60, "Piano"),
+    TRUMPET(54, 86, 57, 69, "Trumpet")
     ;
     
     int lowestNote;
     int highestNote;
     int gmCode;
     int startingPitch;
+    String name;
     
-    InstrumentRange(int lowestNote, int highestNote, int gmCode, int startingPitch){
+    InstrumentRange(int lowestNote, int highestNote, int gmCode, int startingPitch, String name){
         this.lowestNote = lowestNote;
         this.highestNote = highestNote;
         this.gmCode = gmCode;
         this.startingPitch = startingPitch;
+        this.name = name;
     }
 
 }
